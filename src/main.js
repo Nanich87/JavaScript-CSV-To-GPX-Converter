ab4a69db80837cebfaab97c61e7eb80fd151970b
Nanko Geshkov <Nanich87@users.noreply.github.com>
2025-02-02 18:24:47 +0200

    Update

--- src/main.js
+++ src/main.js
@@ -1,4 +1,4 @@
-(function(){
+(function () {
 	var container = document.getElementById('container');
 
 	var inputTitle = document.createElement('h2');
@@ -9,46 +9,46 @@
 
 	var inputBoxPanel = document.createElement('div');
 	inputBoxPanel.className = 'panel panel-info';
-	
+
 	var inputBoxPanelHeading = document.createElement('div');
 	inputBoxPanelHeading.className = 'panel-heading';
-	
+
 	var inputBoxPanelBody = document.createElement('div');
 	inputBoxPanelBody.className = 'panel-body';
-		
+
 	var inputTextArea = document.createElement('textarea');
 	inputTextArea.className = 'input-content form-control';
-	
+
 	var inputDescription = document.createElement('p');
 	inputDescription.innerHTML = 'Example: Point Number,Latitude,Longitude';
-	
+
 	var inputBoxPanelFooter = document.createElement('div');
 	inputBoxPanelFooter.className = 'panel-footer';
 	inputBoxPanelFooter.appendChild(inputDescription);
-	
+
 	inputBoxPanelHeading.appendChild(inputTitle);
 	inputBoxPanelBody.appendChild(inputTextArea);
-	
+
 	inputBoxPanel.appendChild(inputBoxPanelHeading);
 	inputBoxPanel.appendChild(inputBoxPanelBody);
 	inputBoxPanel.appendChild(inputBoxPanelFooter);
 
 	var outputBoxPanel = document.createElement('div');
 	outputBoxPanel.className = 'panel panel-success';
-	
+
 	var outputBoxPanelHeading = document.createElement('div');
 	outputBoxPanelHeading.className = 'panel-heading';
-	
+
 	var outputBoxPanelBody = document.createElement('div');
 	outputBoxPanelBody.className = 'panel-body';
-	
+
 	var outputTextArea = document.createElement('textarea');
 	outputTextArea.className = 'output-content form-control';
 	outputTextArea.setAttribute('readonly', true);
-	
+
 	outputBoxPanelHeading.appendChild(outputTitle);
 	outputBoxPanelBody.appendChild(outputTextArea);
-	
+
 	outputBoxPanel.appendChild(outputBoxPanelHeading);
 	outputBoxPanel.appendChild(outputBoxPanelBody);
 
@@ -66,16 +66,16 @@
 		convert(inputData);
 	}
 
-	function convert(data){
-		if(data == ''){
+	function convert(data) {
+		if (data == '') {
 			return;
 		}
-	
+
 		var d = new Date();
-		var n = d.toISOString(); 
-		
+		var n = d.toISOString();
+
 		outputTextArea.innerHTML = '<?xml version="1.0" encoding="UTF-8" standalone="no" ?>\r\n';
-		outputTextArea.innerHTML += '<gpx xmlns="http://www.topografix.com/GPX/1/1" creator="JavaScript CSV To GPX Converter" version="1.1" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:schemaLocation="http://www.garmin.com/xmlschemas/GpxExtensions/v3 http://www.garmin.com/xmlschemas/GpxExtensions/v3/GpxExtensionsv3.xsd http://www.topografix.com/GPX/1/1 http://www.topografix.com/GPX/1/1/gpx.xsd">\r\n';
+		outputTextArea.innerHTML += '<gpx xmlns="http://www.topografix.com/GPX/1/1" creator="CSV To GPX Converter" version="1.1" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:schemaLocation="http://www.garmin.com/xmlschemas/GpxExtensions/v3 http://www.garmin.com/xmlschemas/GpxExtensions/v3/GpxExtensionsv3.xsd http://www.topografix.com/GPX/1/1 http://www.topografix.com/GPX/1/1/gpx.xsd">\r\n';
 		outputTextArea.innerHTML += '<metadata>\r\n';
 		outputTextArea.innerHTML += ' <link href="http://www.garmin.com">\r\n';
 		outputTextArea.innerHTML += '  <text>Garmin International</text>\r\n';
@@ -83,10 +83,10 @@
 		outputTextArea.innerHTML += ' <time>' + n + '</time>\r\n';
 		outputTextArea.innerHTML += ' <bounds maxlat="0" maxlon="0" minlat="0" minlon="0" />\r\n';
 		outputTextArea.innerHTML += '</metadata>\r\n';
-		
+
 		var output = '';
 
-		for(var i = 0; i < data.length; i++){
+		for (var i = 0; i < data.length; i++) {
 			var line = data[i].trim().replace(/\s\s+/g, ' ').split(",");
 
 			output += ' <wpt lat="' + line[1] + '" lon="' + line[2] + '">\r\n';
